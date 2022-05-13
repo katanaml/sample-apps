3d37664150f7155ddeab2ac66eda026231aecc88
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-05-13 22:52:20 +0300

    Django and React integration

---
+++ 17/django-react/mysite/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './assets/index.js',  // path to our input file
+  output: {
+    filename: 'index-bundle.js',  // output bundle file name
+    path: path.resolve(__dirname, './static'),  // path to our Django static directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+    ]
+  }
+};
